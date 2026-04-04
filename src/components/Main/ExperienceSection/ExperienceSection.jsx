57d2ecb4b967523232e29ec5535d8ff5edcabe85
Dream-World-Coder <dreamy.star.368@gmail.com>
2026-04-05 00:22:00 +0530

    del: remove canvas as it was not fitting the theme

--- src/components/Main/ExperienceSection/ExperienceSection.jsx
+++ src/components/Main/ExperienceSection/ExperienceSection.jsx
@@ -1,12 +1,12 @@
 import Cards from "../../utils/TimeLine";
 import { experienceData } from "./expData";
-import Comp from "@/components/ui/hero-dithering-card.tsx";
+// import Comp from "@/components/ui/hero-dithering-card.tsx";
 
 const ExperienceSection = () => {
   return (
     <section className="experience-section relative w-full h-fit pt-[0vh] md:pt-[7.5vh] lg:pt-[15vh] pb-[25vh] md:pb-[32vh] lg:pb-[40vh]">
-      <Comp>
-        <h4 id="EX" className="text-center mb-8">
+      <div className="py-12 w-full px-12 md:px-16">
+        <h4 id="EX" className="mb-8">
           <span className="exps underline-sp headerSpan header__texts experience--heading">
             Experience
           </span>
@@ -14,7 +14,7 @@ const ExperienceSection = () => {
         <div className="work-container relative w-full h-fit text-xl md:text-2xl">
           <Cards data={experienceData} />
         </div>
-      </Comp>
+      </div>
     </section>
   );
 };
